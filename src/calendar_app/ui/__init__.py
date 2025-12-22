7b6984894102b7fe9e8b00f2f582c567243839df
al-gabriel <al-gabriel@criticalsoftware.com>
2025-12-22 23:03:38 +0000

    Implemented ISSUE_01_03: Basic Month Calendar.

---
+++ src/calendar_app/ui/__init__.py
@@ -0,0 +1,10 @@
+"""
+UI Package
+
+Contains user interface modules for the Calendar App.
+"""
+
+# Import main UI components for easy access
+from .calendar_view import CalendarView
+
+__all__ = ['CalendarView']
